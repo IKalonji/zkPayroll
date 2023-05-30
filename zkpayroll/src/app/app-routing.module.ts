b2440651b3e173480d3e6be36733e02afae5719e
mothupir <mothupir@bbd.co.za>
2023-05-30 19:12:14 +0200

    created auth, employee, and admin component

--- zkpayroll/src/app/app-routing.module.ts
+++ zkpayroll/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/employee/dashboard',
+    pathMatch: 'full'
+  },
+  { 
+    path: 'employee', 
+    loadChildren: () => 
+      import('./employee/employee.module').then(m => m.EmployeeModule) 
+  }, 
+  { 
+    path: 'admin', 
+    loadChildren: () => 
+      import('./admin/admin.module').then(m => m.AdminModule) 
+  }, 
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) 
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
